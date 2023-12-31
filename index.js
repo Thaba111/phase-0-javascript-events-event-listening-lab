8938acac1d67e0284c8a98613240c369178c7a54
Edith Thaba <writersherry5@gmail.com>
2023-12-31 09:50:52 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
-}
+    // Grab the element with the id "input"
+    const input = document.getElementById('input');
+  
+    // Define the callback function
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Add the click event listener
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the function to set up the event listener
+  addingEventListener();
+  
